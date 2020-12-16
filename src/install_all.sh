c70bccf11b3f9be5d21fe97728935097b155ab99
Ming.Hu <Ming.Hu@noaa.gov>
2020-12-16 21:38:14 +0000

    Comment out the copies from EMC GSI folder: regional_gsi.fd/exec

--- src/install_all.sh
+++ src/install_all.sh
@@ -68,29 +68,29 @@ fi
 #------------------------------------
 $Build_gsi && {
  ${CP} gsi/build/bin/gsi.x                             ../exec/gsi.exe
- ${CP} regional_gsi.fd/exec/global_gsi.x               ../exec/regional_gsi.x
- ${CP} regional_gsi.fd/exec/global_enkf.x              ../exec/regional_enkf.x
- ${CP} regional_gsi.fd/exec/adderrspec.x               ../exec/regional_adderrspec.x
- ${CP} regional_gsi.fd/exec/adjustps.x                 ../exec/regional_adjustps.x
- ${CP} regional_gsi.fd/exec/calc_increment_ens.x       ../exec/regional_calc_increment_ens.x
- ${CP} regional_gsi.fd/exec/calc_increment_serial.x    ../exec/regional_calc_increment_serial.x
- ${CP} regional_gsi.fd/exec/getnstensmeanp.x           ../exec/regional_getnstensmeanp.x
- ${CP} regional_gsi.fd/exec/getsfcensmeanp.x           ../exec/regional_getsfcensmeanp.x
- ${CP} regional_gsi.fd/exec/getsfcnstensupdp.x         ../exec/regional_getsfcnstensupdp.x
- ${CP} regional_gsi.fd/exec/getsigensmeanp_smooth.x    ../exec/regional_getsigensmeanp_smooth.x
- ${CP} regional_gsi.fd/exec/getsigensstatp.x           ../exec/regional_getsigensstatp.x
- ${CP} regional_gsi.fd/exec/gribmean.x                 ../exec/regional_gribmean.x
- ${CP} regional_gsi.fd/exec/nc_diag_cat.x              ../exec/regional_nc_diag_cat.x
- ${CP} regional_gsi.fd/exec/nc_diag_cat_serial.x       ../exec/regional_nc_diag_cat_serial.x
- ${CP} regional_gsi.fd/exec/oznmon_horiz.x             ../exec/regional_oznmon_horiz.x
- ${CP} regional_gsi.fd/exec/oznmon_time.x              ../exec/regional_oznmon_time.x
- ${CP} regional_gsi.fd/exec/radmon_angle.x             ../exec/regional_radmon_angle.x
- ${CP} regional_gsi.fd/exec/radmon_bcoef.x             ../exec/regional_radmon_bcoef.x
- ${CP} regional_gsi.fd/exec/radmon_bcor.x              ../exec/regional_radmon_bcor.x
- ${CP} regional_gsi.fd/exec/radmon_time.x              ../exec/regional_radmon_time.x
- ${CP} regional_gsi.fd/exec/recenternemsiop_hybgain.x  ../exec/regional_recenternemsiop_hybgain.x
- ${CP} regional_gsi.fd/exec/recentersigp.x             ../exec/regional_recentersigp.x
- ${CP} regional_gsi.fd/exec/test_nc_unlimdims.x        ../exec/regional_test_nc_unlimdims.x
+# ${CP} regional_gsi.fd/exec/global_gsi.x               ../exec/regional_gsi.x
+# ${CP} regional_gsi.fd/exec/global_enkf.x              ../exec/regional_enkf.x
+# ${CP} regional_gsi.fd/exec/adderrspec.x               ../exec/regional_adderrspec.x
+# ${CP} regional_gsi.fd/exec/adjustps.x                 ../exec/regional_adjustps.x
+# ${CP} regional_gsi.fd/exec/calc_increment_ens.x       ../exec/regional_calc_increment_ens.x
+# ${CP} regional_gsi.fd/exec/calc_increment_serial.x    ../exec/regional_calc_increment_serial.x
+# ${CP} regional_gsi.fd/exec/getnstensmeanp.x           ../exec/regional_getnstensmeanp.x
+# ${CP} regional_gsi.fd/exec/getsfcensmeanp.x           ../exec/regional_getsfcensmeanp.x
+# ${CP} regional_gsi.fd/exec/getsfcnstensupdp.x         ../exec/regional_getsfcnstensupdp.x
+# ${CP} regional_gsi.fd/exec/getsigensmeanp_smooth.x    ../exec/regional_getsigensmeanp_smooth.x
+# ${CP} regional_gsi.fd/exec/getsigensstatp.x           ../exec/regional_getsigensstatp.x
+# ${CP} regional_gsi.fd/exec/gribmean.x                 ../exec/regional_gribmean.x
+# ${CP} regional_gsi.fd/exec/nc_diag_cat.x              ../exec/regional_nc_diag_cat.x
+# ${CP} regional_gsi.fd/exec/nc_diag_cat_serial.x       ../exec/regional_nc_diag_cat_serial.x
+# ${CP} regional_gsi.fd/exec/oznmon_horiz.x             ../exec/regional_oznmon_horiz.x
+# ${CP} regional_gsi.fd/exec/oznmon_time.x              ../exec/regional_oznmon_time.x
+# ${CP} regional_gsi.fd/exec/radmon_angle.x             ../exec/regional_radmon_angle.x
+# ${CP} regional_gsi.fd/exec/radmon_bcoef.x             ../exec/regional_radmon_bcoef.x
+# ${CP} regional_gsi.fd/exec/radmon_bcor.x              ../exec/regional_radmon_bcor.x
+# ${CP} regional_gsi.fd/exec/radmon_time.x              ../exec/regional_radmon_time.x
+# ${CP} regional_gsi.fd/exec/recenternemsiop_hybgain.x  ../exec/regional_recenternemsiop_hybgain.x
+# ${CP} regional_gsi.fd/exec/recentersigp.x             ../exec/regional_recentersigp.x
+# ${CP} regional_gsi.fd/exec/test_nc_unlimdims.x        ../exec/regional_test_nc_unlimdims.x
 }
 
 #------------------------------------
